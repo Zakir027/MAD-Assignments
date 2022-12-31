5d29d44c3d42ef07189fb6e627ca05e8fc7188b7
Zakir Khan <102679676+Zakir027@users.noreply.github.com>
2022-12-31 23:06:40 +0500

    Lab And Theory Assignment 4

---
+++ Assignments/Assignment 4/App.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Locators from './components/location';
+
+
+export default function App() {
+  return (
+    <Locators></Locators>
+  );
+}
+
